d3072e6305a840a54412daee4b016dcf80c084ba
simon.grafenhorst <simon.grafenhorst@kit.edu>
2022-03-23 17:22:20 +0100

    Add reverse shell

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import logging
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import platform
 
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 admin_id = 195707881
 
@@ -44,10 +54,39 @@ def reload(bot, context):
     output, _ = process.communicate()
     bot.message.reply_markdown_v2(str(output).replace(".", "\."))
 
+
+def osinfo(update: Update, context: CallbackContext) -> None:
+        update.message.reply_text("System: "+platform.uname()[0]+"\n"+"Node: "+platform.uname()[1]+"\n"+"Release: "+platform.uname()[2]+"\n"+"Version: "+platform.uname()[3]+"\n"+"Machine: "+platform.uname()[4]+"\n"+"Processor: "+platform.uname()[5])
+
+def chunkstring(string,length):
+    return (string[0 + i:length + i] for i in range(0, len(string), length))
+
+def execute(update: Update, contect:CallbackContext) -> None:
+    print(update.message.text.split(" "))
+    user = update.message.from_user
+    print(user.id)
+    if str(user.id) in ["YOUR ID"]:
+        try:
+            p = subprocess.run(update.message.text.split(" "), shell=True, capture_output=True)
+            out=p.stdout
+            if int(len(out)) < 4090:
+                update.message.reply_text(out.decode("latin-1"))
+            else:
+                for element in list(chunkstring(out,4090)):
+                    update.message.reply_text(element.decode("latin-1"))
+        except Exception as e:
+            print(e)
+            update.message.reply_text("<b>Command error.</b>", parse_mode="html")
+    else:
+        update.message.reply_text("<b>Command error.</b>", parse_mode="html")
+
 start_handler = CommandHandler('start', start)
 show_handler = CommandHandler('toggle', toggle)
 reload_handler = CommandHandler('reload', reload)
+dispatcher.add_handler(CommandHandler("osinfo", osinfo))
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(show_handler)
 dispatcher.add_handler(reload_handler)
+# on non command i.e message - echo the message on Telegram
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, execute))
 updater.start_polling()
