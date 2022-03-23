ff6ec64626d03a5b7f63b0ba646dccde02b8ce16
simon.grafenhorst <simon.grafenhorst@kit.edu>
2022-03-23 14:23:49 +0100

    Implement pumps

--- main.py
+++ main.py
@@ -16,17 +16,7 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(19, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 
-while True:
-    GPIO.output(16, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
-    GPIO.output(19, GPIO.HIGH)
-    GPIO.output(21, GPIO.HIGH)
-    time.sleep(5)
-    GPIO.output(16, GPIO.LOW)
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(19, GPIO.LOW)
-    GPIO.output(21, GPIO.LOW)
-    time.sleep(5)
+pump_array = [16, 18, 19, 21]
 
 
 
@@ -36,16 +26,20 @@ dispatcher = updater.dispatcher
 bot_obj = updater.bot
 
 
-def start(bot, update):
-    print(update.effective_user.id)
+def start(bot, context):
+    print(context.effective_user.id)
 
-def show(bot, update):
-    msg = "hallo test"
-    if(update.message.chat.id == 195707881):
-        bot.send_message(chat_id=update.message.chat_id, disable_web_page_preview=True, parse_mode="markdown", text=msg)
+def toggle(bot, context):
+    if(len(context.args) == 0):
+        return
+    GPIO.output(pump_array[context.args[0]], GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(pump_array[context.args[0]], GPIO.LOW)
+    bot.message.reply_markdown_v2(
+        "Pump run: " + str(pump_array[context.args[0]]))
 
 start_handler = CommandHandler('start', start)
-show_handler = CommandHandler('show', show)
+show_handler = CommandHandler('toggle', toggle)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(show_handler)
 updater.start_polling()
